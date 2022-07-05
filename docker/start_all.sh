2c5c6bce665209bcde4377ae823902129314e253
imrtstore <rtstrore_dev@outlook.com>
2022-07-05 13:17:46 +0800

    fix: move rtstore to db3 in docker/start_all.sh

--- docker/start_all.sh
+++ docker/start_all.sh
@@ -13,26 +13,26 @@ mkdir -p /data
 cd /minio_dir && chmod +x minio && ./minio server /data  --console-address ":9001" > minio.log 2>&1 &
 sleep 2
 
-echo "start rtstore "
-cd /rtstore_dir && chmod +x rtstore
+echo "start db3 "
+cd /db3_dir && chmod +x db3
 export AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMPLE
 export AWS_SECRET_ACCESS_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
-RUST_LOG=info ./rtstore meta 9191 http://127.0.0.1:2379 /rtstore 127.0.0.1 http://127.0.0.1:9000 > meta.log 2>&1 &
+RUST_LOG=info ./db3 meta 9191 http://127.0.0.1:2379 /db3 127.0.0.1 http://127.0.0.1:9000 > meta.log 2>&1 &
 sleep 1
 tail -n 100 meta.log
-RUST_LOG=info ./rtstore memory-node 9791 /tmp/binlog /tmp/test http://127.0.0.1:2379 /rtstore 127.0.0.1 > mem.log 2>&1 &
+RUST_LOG=info ./db3 memory-node 9791 /tmp/binlog /tmp/test http://127.0.0.1:2379 /db3 127.0.0.1 > mem.log 2>&1 &
 sleep 1
 tail -n 100 mem.log
-RUST_LOG=info ./rtstore compute-node 9193 http://127.0.0.1:2379 /rtstore 127.0.0.1 http://127.0.0.1:9000 > compute.log 2>&1 &
+RUST_LOG=info ./db3 compute-node 9193 http://127.0.0.1:2379 /db3 127.0.0.1 http://127.0.0.1:9000 > compute.log 2>&1 &
 sleep 1
 tail -n 100 compute.log
-RUST_LOG=info ./rtstore frontend-node 9292 http://127.0.0.1:2379 /rtstore 0.0.0.0 ./vars.txt > fe.log 2>&1 &
+RUST_LOG=info ./db3 frontend-node 9292 http://127.0.0.1:2379 /db3 0.0.0.0 ./vars.txt > fe.log 2>&1 &
 sleep 1
 tail -n 100 fe.log
 
-echo "start rtstore done"
+echo "start db3 done"
 
-echo "You can use 'mysql -u root -h 127.0.0.1 -P 9292 ' to connect to rtstore"
+echo "You can use 'mysql -u root -h 127.0.0.1 -P 9292 ' to connect to db3"
 while true; do sleep 1; done
 
 
