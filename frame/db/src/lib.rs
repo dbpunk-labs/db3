80d8178bbcef2a4235dcd6f7a557c18ab95eebdb
imrtstore <rtstrore_dev@outlook.com>
2022-09-22 11:49:21 +0800

    fix: remove unused code

--- frame/db/src/lib.rs
+++ frame/db/src/lib.rs
@@ -24,8 +24,6 @@ pub mod weights;
 
 #[cfg(test)]
 mod mock;
-#[cfg(test)]
-mod tests;
 
 use codec::{Decode, Encode, MaxEncodedLen};
 use frame_support::{
@@ -80,33 +78,6 @@ pub use weights::WeightInfo;
 pub const DEFAULT_MAX_TRANSACTION_SIZE: u32 = 8 * 1024 * 1024;
 pub const DEFAULT_MAX_BLOCK_TRANSACTIONS: u32 = 512;
 const SQL_KEY: &[u8] = b"sql_key";
-
-/// State data for a stored transaction.
-#[derive(
-    Encode,
-    Decode,
-    Clone,
-    sp_runtime::RuntimeDebug,
-    PartialEq,
-    Eq,
-    scale_info::TypeInfo,
-    MaxEncodedLen,
-)]
-pub struct TransactionInfo {
-    /// Chunk trie root.
-    chunk_root: <BlakeTwo256 as Hash>::Output,
-    /// Plain hash of indexed data.
-    content_hash: <BlakeTwo256 as Hash>::Output,
-    /// Size of indexed data in bytes.
-    size: u32,
-    /// Total number of chunks added in the block with this transaction. This
-    /// is used find transaction info by block chunk index using binary search.
-    block_chunks: u32,
-}
-
-fn num_chunks(bytes: u32) -> u32 {
-    ((bytes as u64 + CHUNK_SIZE as u64 - 1) / CHUNK_SIZE as u64) as u32
-}
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 struct SQLInput<'a> {
@@ -218,38 +189,11 @@ pub mod pallet {
     #[pallet::hooks]
     impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
         fn on_initialize(n: T::BlockNumber) -> Weight {
-            // Drop obsolete roots. The proof for `obsolete` will be checked later
-            // in this block, so we drop `obsolete` - 1.
-            let period = <StoragePeriod<T>>::get();
-            let obsolete = n.saturating_sub(period.saturating_add(One::one()));
-            if obsolete > Zero::zero() {
-                <Transactions<T>>::remove(obsolete);
-                <ChunkCount<T>>::remove(obsolete);
-            }
+            // clear the pending sql queue
             PendingSQL::<T>::kill();
-            // 2 writes in `on_initialize` and 2 writes + 2 reads in `on_finalize`
-            T::DbWeight::get().reads_writes(2, 4)
         }
 
-        fn on_finalize(n: T::BlockNumber) {
-            assert!(
-                <ProofChecked<T>>::take() || {
-                    // Proof is not required for early or empty blocks.
-                    let number = <frame_system::Pallet<T>>::block_number();
-                    let period = <StoragePeriod<T>>::get();
-                    let target_number = number.saturating_sub(period);
-                    target_number.is_zero() || <ChunkCount<T>>::get(target_number) == 0
-                },
-                "Storage proof must be checked once in the block"
-            );
-            // Insert new transactions
-            let transactions = <BlockTransactions<T>>::take();
-            let total_chunks = transactions.last().map_or(0, |t| t.block_chunks);
-            if total_chunks != 0 {
-                <ChunkCount<T>>::insert(n, total_chunks);
-                <Transactions<T>>::insert(n, transactions);
-            }
-        }
+        fn on_finalize(n: T::BlockNumber) {}
 
         fn offchain_worker(_n: T::BlockNumber) {
             //TODO use batch to sumit transaction
@@ -260,9 +204,9 @@ pub mod pallet {
                 let raw_id_data = req_id.to_vec();
                 let req_id = str::from_utf8(&raw_id_data).unwrap_or("error");
                 let mut data = id.encode();
+                // combie the account id and namespace
                 data.extend(ns_raw.iter());
                 let account = hex::encode_upper(data);
-                log::info!("process sql {} offchain from acc {} ", query, account);
                 Self::run_and_send_signed_payload(query, &account, req_id).unwrap();
             }
         }
@@ -270,13 +214,6 @@ pub mod pallet {
 
     #[pallet::call]
     impl<T: Config> Pallet<T> {
-        #[pallet::weight(10000)]
-        pub fn submit_unsigned_result(origin: OriginFor<T>, data: Vec<u8>) -> DispatchResult {
-            ensure_none(origin)?;
-            Self::deposit_event(Event::SQLResult(data));
-            Ok(())
-        }
-
         #[pallet::weight(10000)]
         pub fn submit_result(origin: OriginFor<T>, data: InputPayload) -> DispatchResult {
             ensure_signed(origin)?;
@@ -549,168 +486,11 @@ pub mod pallet {
                 Ok(())
             }
         }
-
-        /// Index and store data off chain. Minimum data size is 1 bytes, maximum is
-        /// `MaxTransactionSize`. Data will be removed after `STORAGE_PERIOD` blocks, unless `renew`
-        /// is called. # <weight>
-        /// - n*log(n) of data size, as all data is pushed to an in-memory trie.
-        /// Additionally contains a DB write.
-        /// # </weight>
-        #[pallet::weight(T::WeightInfo::store(data.len() as u32))]
-        pub fn store(origin: OriginFor<T>, data: Vec<u8>) -> DispatchResult {
-            ensure!(data.len() > 0, Error::<T>::EmptyTransaction);
-            ensure!(
-                data.len() <= T::MaxTransactionSize::get() as usize,
-                Error::<T>::TransactionTooLarge
-            );
-            let sender = ensure_signed(origin)?;
-            Self::apply_fee(sender, data.len() as u32)?;
-
-            // Chunk data and compute storage root
-            let chunk_count = num_chunks(data.len() as u32);
-            let chunks = data.chunks(CHUNK_SIZE).map(|c| c.to_vec()).collect();
-            let root = sp_io::trie::blake2_256_ordered_root(chunks, sp_runtime::StateVersion::V1);
-
-            let content_hash = sp_io::hashing::blake2_256(&data);
-            let extrinsic_index =
-                <frame_system::Pallet<T>>::extrinsic_index().ok_or(Error::<T>::BadContext)?;
-            sp_io::transaction_index::index(extrinsic_index, data.len() as u32, content_hash);
-
-            let mut index = 0;
-            <BlockTransactions<T>>::mutate(|transactions| {
-                if transactions.len() + 1 > T::MaxBlockTransactions::get() as usize {
-                    return Err(Error::<T>::TooManyTransactions);
-                }
-                let total_chunks = transactions.last().map_or(0, |t| t.block_chunks) + chunk_count;
-                index = transactions.len() as u32;
-                transactions
-                    .try_push(TransactionInfo {
-                        chunk_root: root,
-                        size: data.len() as u32,
-                        content_hash: content_hash.into(),
-                        block_chunks: total_chunks,
-                    })
-                    .map_err(|_| Error::<T>::TooManyTransactions)?;
-                Ok(())
-            })?;
-            Self::deposit_event(Event::Stored { index });
-            Ok(())
-        }
-
-        /// Renew previously stored data. Parameters are the block number that contains
-        /// previous `store` or `renew` call and transaction index within that block.
-        /// Transaction index is emitted in the `Stored` or `Renewed` event.
-        /// Applies same fees as `store`.
-        /// # <weight>
-        /// - Constant.
-        /// # </weight>
-        #[pallet::weight(T::WeightInfo::renew())]
-        pub fn renew(
-            origin: OriginFor<T>,
-            block: T::BlockNumber,
-            index: u32,
-        ) -> DispatchResultWithPostInfo {
-            let sender = ensure_signed(origin)?;
-            let transactions = <Transactions<T>>::get(block).ok_or(Error::<T>::RenewedNotFound)?;
-            let info = transactions
-                .get(index as usize)
-                .ok_or(Error::<T>::RenewedNotFound)?;
-            let extrinsic_index =
-                <frame_system::Pallet<T>>::extrinsic_index().ok_or(Error::<T>::BadContext)?;
-            Self::apply_fee(sender, info.size)?;
-            sp_io::transaction_index::renew(extrinsic_index, info.content_hash.into());
-
-            let mut index = 0;
-            <BlockTransactions<T>>::mutate(|transactions| {
-                if transactions.len() + 1 > T::MaxBlockTransactions::get() as usize {
-                    return Err(Error::<T>::TooManyTransactions);
-                }
-                let chunks = num_chunks(info.size);
-                let total_chunks = transactions.last().map_or(0, |t| t.block_chunks) + chunks;
-                index = transactions.len() as u32;
-                transactions
-                    .try_push(TransactionInfo {
-                        chunk_root: info.chunk_root,
-                        size: info.size,
-                        content_hash: info.content_hash,
-                        block_chunks: total_chunks,
-                    })
-                    .map_err(|_| Error::<T>::TooManyTransactions)
-            })?;
-            Self::deposit_event(Event::Renewed { index });
-            Ok(().into())
-        }
-
-        /// Check storage proof for block number `block_number() - StoragePeriod`.
-        /// If such block does not exist the proof is expected to be `None`.
-        /// # <weight>
-        /// - Linear w.r.t the number of indexed transactions in the proved block for random
-        ///   probing.
-        /// There's a DB read for each transaction.
-        /// Here we assume a maximum of 100 probed transactions.
-        /// # </weight>
-        #[pallet::weight((T::WeightInfo::check_proof_max(), DispatchClass::Mandatory))]
-        pub fn check_proof(
-            origin: OriginFor<T>,
-            proof: TransactionStorageProof,
-        ) -> DispatchResultWithPostInfo {
-            ensure_none(origin)?;
-            ensure!(!ProofChecked::<T>::get(), Error::<T>::DoubleCheck);
-            let number = <frame_system::Pallet<T>>::block_number();
-            let period = <StoragePeriod<T>>::get();
-            let target_number = number.saturating_sub(period);
-            ensure!(!target_number.is_zero(), Error::<T>::UnexpectedProof);
-            let total_chunks = <ChunkCount<T>>::get(target_number);
-            ensure!(total_chunks != 0, Error::<T>::UnexpectedProof);
-            let parent_hash = <frame_system::Pallet<T>>::parent_hash();
-            let selected_chunk_index = random_chunk(parent_hash.as_ref(), total_chunks);
-            let (info, chunk_index) = match <Transactions<T>>::get(target_number) {
-                Some(infos) => {
-                    let index = match infos
-                        .binary_search_by_key(&selected_chunk_index, |info| info.block_chunks)
-                    {
-                        Ok(index) => index,
-                        Err(index) => index,
-                    };
-                    let info = infos
-                        .get(index)
-                        .ok_or(Error::<T>::MissingStateData)?
-                        .clone();
-                    let chunks = num_chunks(info.size);
-                    let prev_chunks = info.block_chunks - chunks;
-                    (info, selected_chunk_index - prev_chunks)
-                }
-                None => return Err(Error::<T>::MissingStateData.into()),
-            };
-            ensure!(
-                sp_io::trie::blake2_256_verify_proof(
-                    info.chunk_root,
-                    &proof.proof,
-                    &encode_index(chunk_index),
-                    &proof.chunk,
-                    sp_runtime::StateVersion::V1,
-                ),
-                Error::<T>::InvalidProof
-            );
-            ProofChecked::<T>::put(true);
-            Self::deposit_event(Event::ProofChecked);
-            Ok(().into())
-        }
     }
 
     #[pallet::event]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
     pub enum Event<T: Config> {
-        /// Stored data under specified index.
-        Stored {
-            index: u32,
-        },
-        /// Renewed data under specified index.
-        Renewed {
-            index: u32,
-        },
-        /// Storage proof was successfully checked.
-        ProofChecked,
         SQLQueued(Vec<u8>),
         SQLResult(Vec<u8>),
         AddDelegateOK(Vec<u8>),
@@ -724,22 +504,6 @@ pub mod pallet {
         },
         NoNsPermission,
     }
-
-    /// Collection of transaction metadata by block number.
-    #[pallet::storage]
-    #[pallet::getter(fn transaction_roots)]
-    pub(super) type Transactions<T: Config> = StorageMap<
-        _,
-        Blake2_128Concat,
-        T::BlockNumber,
-        BoundedVec<TransactionInfo, T::MaxBlockTransactions>,
-        OptionQuery,
-    >;
-
-    /// Count indexed chunks for each block.
-    #[pallet::storage]
-    pub(super) type ChunkCount<T: Config> =
-        StorageMap<_, Blake2_128Concat, T::BlockNumber, u32, ValueQuery>;
 
     #[pallet::storage]
     #[pallet::getter(fn table_owners)]
@@ -761,30 +525,6 @@ pub mod pallet {
             T::MaxBlockTransactions,
         >,
     >;
-
-    #[pallet::storage]
-    #[pallet::getter(fn byte_fee)]
-    /// Storage fee per byte.
-    pub(super) type ByteFee<T: Config> = StorageValue<_, BalanceOf<T>>;
-
-    #[pallet::storage]
-    #[pallet::getter(fn entry_fee)]
-    /// Storage fee per transaction.
-    pub(super) type EntryFee<T: Config> = StorageValue<_, BalanceOf<T>>;
-
-    /// Storage period for data in blocks. Should match `sp_storage_proof::DEFAULT_STORAGE_PERIOD`
-    /// for block authoring.
-    #[pallet::storage]
-    pub(super) type StoragePeriod<T: Config> = StorageValue<_, T::BlockNumber, ValueQuery>;
-
-    // Intermediates
-    #[pallet::storage]
-    pub(super) type BlockTransactions<T: Config> =
-        StorageValue<_, BoundedVec<TransactionInfo, T::MaxBlockTransactions>, ValueQuery>;
-
-    /// Was the proof checked in this block?
-    #[pallet::storage]
-    pub(super) type ProofChecked<T: Config> = StorageValue<_, bool, ValueQuery>;
 
     #[pallet::storage]
     pub(super) type PendingSQL<T: Config> = StorageValue<
@@ -825,31 +565,6 @@ pub mod pallet {
             <ByteFee<T>>::put(&self.byte_fee);
             <EntryFee<T>>::put(&self.entry_fee);
             <StoragePeriod<T>>::put(&self.storage_period);
-        }
-    }
-
-    #[pallet::inherent]
-    impl<T: Config> ProvideInherent for Pallet<T> {
-        type Call = Call<T>;
-        type Error = InherentError;
-        const INHERENT_IDENTIFIER: InherentIdentifier = INHERENT_IDENTIFIER;
-
-        fn create_inherent(data: &InherentData) -> Option<Self::Call> {
-            let proof = data
-                .get_data::<TransactionStorageProof>(&Self::INHERENT_IDENTIFIER)
-                .unwrap_or(None);
-            proof.map(|proof| Call::check_proof { proof })
-        }
-
-        fn check_inherent(
-            _call: &Self::Call,
-            _data: &InherentData,
-        ) -> result::Result<(), Self::Error> {
-            Ok(())
-        }
-
-        fn is_inherent(call: &Self::Call) -> bool {
-            matches!(call, Call::check_proof { .. })
         }
     }
 
