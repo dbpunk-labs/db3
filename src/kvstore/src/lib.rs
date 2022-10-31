4c99852d71612fab203f38c4bc8f7f51b5227113
Chen22 <jingchen2222@gmail.com>
2022-10-30 17:07:48 -0700

    add bill logic 1. accumulate bill when insert kv store 2. support query with bill

--- src/kvstore/src/lib.rs
+++ src/kvstore/src/lib.rs
@@ -17,12 +17,15 @@ use tendermint_proto::abci::{
     ResponseCommit, ResponseDeliverTx, ResponseInfo, ResponseQuery,
 };
 use tracing::{debug, info};
+use db3_crypto::account_id::AccountId;
+
+pub const UNIT_CONST: i64 = 1;
+
 /// In-memory, hashmap-backed key/value store ABCI application.
 ///
 /// This structure effectively just serves as a handle to the actual key/value
 /// store - the [`KeyValueStoreDriver`].
 ///
-
 #[derive(Debug, Clone)]
 pub struct KeyValueStoreApp {
     cmd_tx: Sender<Command>,
@@ -33,6 +36,20 @@ impl KeyValueStoreApp {
     pub fn new() -> (Self, KeyValueStoreDriver) {
         let (cmd_tx, cmd_rx) = channel();
         (Self { cmd_tx }, KeyValueStoreDriver::new(cmd_rx))
+    }
+
+
+    /// Attempt to retrieve the bill associated with the given addr.
+    pub fn get_bill(&self, addr: String) -> Result<(i64, Option<i64>), Error> {
+        let (result_tx, result_rx) = channel();
+        channel_send(
+            &self.cmd_tx,
+            Command::GetBill {
+                addr: addr,
+                result_tx,
+            },
+        )?;
+        channel_recv(&result_rx)
     }
 
     /// Attempt to retrieve the value associated with the given key.
@@ -52,15 +69,16 @@ impl KeyValueStoreApp {
     ///
     /// Optionally returns any pre-existing value associated with the given
     /// key.
-    pub fn set<K, V>(&self, key: K, value: V) -> Result<Option<String>, Error>
-    where
-        K: AsRef<str>,
-        V: AsRef<str>,
+    pub fn set<K, V>(&self, addr: &str, key: K, value: V) -> Result<Option<String>, Error>
+        where
+            K: AsRef<str>,
+            V: AsRef<str>,
     {
         let (result_tx, result_rx) = channel();
         channel_send(
             &self.cmd_tx,
             Command::Set {
+                addr: addr.to_string(),
                 key: key.as_ref().to_string(),
                 value: value.as_ref().to_string(),
                 result_tx,
@@ -90,38 +108,86 @@ impl Application for KeyValueStoreApp {
         }
     }
 
+    ///
+    /// Query with path and data
+    /// query /store
+    /// query /bill
     fn query(&self, request: RequestQuery) -> ResponseQuery {
-        let key = match String::from_utf8(request.data.clone()) {
-            Ok(s) => s,
-            Err(e) => panic!("Failed to intepret key as UTF-8: {}", e),
-        };
-        debug!("Attempting to get key: {}", key);
-        match self.get(key.clone()) {
-            Ok((height, value_opt)) => match value_opt {
-                Some(value) => ResponseQuery {
-                    code: 0,
-                    log: "exists".to_string(),
-                    info: "".to_string(),
-                    index: 0,
-                    key: request.data,
-                    value: value.into_bytes(),
-                    proof_ops: None,
-                    height,
-                    codespace: "".to_string(),
+        let formatted_paht = request.path.to_lowercase();
+        let tokens = formatted_paht.split('/').collect::<Vec<&str>>();
+
+
+        if tokens.len() > 0 && tokens[0] == "bill" {
+            // query bill
+            let addr = match String::from_utf8(request.data.clone()) {
+                Ok(s) => s,
+                Err(e) => panic!("Failed to intepret key as UTF-8: {}", e),
+            };
+            debug!("Attempting to get bill for : {}", addr);
+            match self.get_bill(addr.clone()) {
+                Ok((height, value_opt)) => match value_opt {
+                    Some(value) => {
+                        println!("bill: {}", value);
+                        ResponseQuery {
+                            code: 0,
+                            log: "exists".to_string(),
+                            info: "".to_string(),
+                            index: 0,
+                            key: request.data,
+                            value: value.to_string().into_bytes(),
+                            proof_ops: None,
+                            height,
+                            codespace: "".to_string(),
+                        }
+                    }
+                    None => ResponseQuery {
+                        code: 0,
+                        log: "bill does not exist".to_string(),
+                        info: "".to_string(),
+                        index: 0,
+                        key: request.data,
+                        value: vec![],
+                        proof_ops: None,
+                        height,
+                        codespace: "".to_string(),
+                    },
                 },
-                None => ResponseQuery {
-                    code: 0,
-                    log: "does not exist".to_string(),
-                    info: "".to_string(),
-                    index: 0,
-                    key: request.data,
-                    value: vec![],
-                    proof_ops: None,
-                    height,
-                    codespace: "".to_string(),
+                Err(e) => panic!("Failed to get bill for \"{}\": {:?}", addr, e),
+            }
+        } else {
+            // query store
+            let key = match String::from_utf8(request.data.clone()) {
+                Ok(s) => s,
+                Err(e) => panic!("Failed to intepret key as UTF-8: {}", e),
+            };
+            debug!("Attempting to get key: {}", key);
+            match self.get(key.clone()) {
+                Ok((height, value_opt)) => match value_opt {
+                    Some(value) => ResponseQuery {
+                        code: 0,
+                        log: "exists".to_string(),
+                        info: "".to_string(),
+                        index: 0,
+                        key: request.data,
+                        value: value.into_bytes(),
+                        proof_ops: None,
+                        height,
+                        codespace: "".to_string(),
+                    },
+                    None => ResponseQuery {
+                        code: 0,
+                        log: "key does not exist".to_string(),
+                        info: "".to_string(),
+                        index: 0,
+                        key: request.data,
+                        value: vec![],
+                        proof_ops: None,
+                        height,
+                        codespace: "".to_string(),
+                    },
                 },
-            },
-            Err(e) => panic!("Failed to get key \"{}\": {:?}", key, e),
+                Err(e) => panic!("Failed to get key \"{}\": {:?}", key, e),
+            }
         }
     }
 
@@ -161,10 +227,30 @@ impl Application for KeyValueStoreApp {
         let buf = hex::decode(tx).unwrap();
         let request = WriteRequest::decode(buf.as_ref()).unwrap();
         let mutation = Mutation::decode(request.mutation.as_ref()).unwrap();
+        let account_id = verifier::MutationVerifier::verify(&request).unwrap();
+        let addr = hex::encode(account_id.addr.as_bytes());
+        println!("addr: {}", addr);
         for kv_pair in mutation.kv_pairs {
             let key = String::from_utf8(kv_pair.key).unwrap();
             let value = String::from_utf8(kv_pair.value).unwrap();
-            let _ = self.set(key, value).unwrap();
+            match self.set(&addr, key, value) {
+                Ok(res) => {}
+                Err(e) => {
+                    return ResponseDeliverTx {
+                        code: 1,
+                        data: vec![],
+                        log: "".to_string(),
+                        info: "".to_string(),
+                        gas_wanted: 0,
+                        gas_used: 0,
+                        events: vec![Event {
+                            r#type: "app".to_string(),
+                            attributes: vec![],
+                        }],
+                        codespace: "".to_string(),
+                    };
+                }
+            }
         }
         ResponseDeliverTx {
             code: 0,
@@ -197,6 +283,7 @@ impl Application for KeyValueStoreApp {
 #[derive(Debug)]
 pub struct KeyValueStoreDriver {
     store: HashMap<String, String>,
+    account_bill: HashMap<String, i64>,
     height: i64,
     app_hash: Vec<u8>,
     cmd_rx: Receiver<Command>,
@@ -206,6 +293,7 @@ impl KeyValueStoreDriver {
     fn new(cmd_rx: Receiver<Command>) -> Self {
         Self {
             store: HashMap::new(),
+            account_bill: HashMap::new(),
             height: 0,
             app_hash: vec![0_u8; MAX_VARINT_LENGTH],
             cmd_rx,
@@ -220,6 +308,13 @@ impl KeyValueStoreDriver {
                 Command::GetInfo { result_tx } => {
                     channel_send(&result_tx, (self.height, self.app_hash.clone()))?
                 }
+                Command::GetBill { addr, result_tx } => {
+                    debug!("Getting bill for \"{}\"", addr);
+                    channel_send(
+                        &result_tx,
+                        (self.height, self.account_bill.get(&addr).map(Clone::clone)),
+                    )?;
+                }
                 Command::Get { key, result_tx } => {
                     debug!("Getting value for \"{}\"", key);
                     channel_send(
@@ -228,12 +323,20 @@ impl KeyValueStoreDriver {
                     )?;
                 }
                 Command::Set {
+                    addr,
                     key,
                     value,
                     result_tx,
                 } => {
                     debug!("Setting \"{}\" = \"{}\"", key, value);
-                    channel_send(&result_tx, self.store.insert(key, value))?;
+                    let value_size = value.as_bytes().len() as i64;
+                    let mut payload_cost = value_size * UNIT_CONST;
+                    if let Some(cost) = self.account_bill.get(&addr) {
+                        payload_cost += cost;
+                    }
+                    println!("account bill insert {} {}", addr.clone(), payload_cost);
+                    self.account_bill.insert(addr, payload_cost);
+                    channel_send(&result_tx, self.store.insert(key, value));
                 }
                 Command::Commit { result_tx } => self.commit(result_tx)?,
             }
@@ -244,7 +347,7 @@ impl KeyValueStoreDriver {
         // As in the Go-based key/value store, simply encode the number of
         // items as the "app hash"
         let mut app_hash = BytesMut::with_capacity(MAX_VARINT_LENGTH);
-        codec::encode_varint(self.store.len() as u64, &mut app_hash);
+        codec::encode_varint(self.store.len() as u64 + self.account_bill.len() as u64, &mut app_hash);
         self.app_hash = app_hash.to_vec();
         self.height += 1;
         channel_send(&result_tx, (self.height, self.app_hash.clone()))
@@ -255,6 +358,10 @@ impl KeyValueStoreDriver {
 enum Command {
     /// Get the height of the last commit.
     GetInfo { result_tx: Sender<(i64, Vec<u8>)> },
+    GetBill {
+        addr: String,
+        result_tx: Sender<(i64, Option<i64>)>,
+    },
     /// Get the key associated with `key`.
     Get {
         key: String,
@@ -262,6 +369,7 @@ enum Command {
     },
     /// Set the value of `key` to to `value`.
     Set {
+        addr: String,
         key: String,
         value: String,
         result_tx: Sender<Option<String>>,
--- src/kvstore/tests/kvstore_app.rs
+++ src/kvstore/tests/kvstore_app.rs
@@ -70,5 +70,128 @@ mod kvstore_app_integration {
             })
             .unwrap();
         assert_eq!(res.value, "value1".as_bytes().to_owned());
+
+        let res = client
+            .query(RequestQuery {
+                data: "unknown".as_bytes().to_owned(),
+                path: "".to_string(),
+                height: 0,
+                prove: false,
+            })
+            .unwrap();
+        assert_eq!(res.value, []);
+
+        let res = client
+            .query(RequestQuery {
+                data: "0dce49e41905e6c0c5091adcedee2dee524a3b06".as_bytes().to_owned(),
+                path: "bill".to_string(),
+                height: 0,
+                prove: false,
+            })
+            .unwrap();
+        assert_eq!(res.value, "6".as_bytes().to_owned());
+        let res = client
+            .query(RequestQuery {
+                data: "1234".as_bytes().to_owned(),
+                path: "bill".to_string(),
+                height: 0,
+                prove: false,
+            })
+            .unwrap();
+        assert_eq!(res.code, 0);
+        assert_eq!(res.value, []);
+        assert_eq!(res.log, "bill does not exist");
+    }
+
+    #[test]
+    fn multi_kv_pair_happy_path() {
+        let mut rng = StdRng::from_seed([0; 32]);
+        let kp = Secp256k1KeyPair::generate(&mut rng);
+        let mutation = Mutation {
+            ns: "my_twitter".as_bytes().to_vec(),
+            kv_pairs: vec![
+                KvPair {
+                    key: "k1".as_bytes().to_vec(),
+                    value: "value1".as_bytes().to_vec(),
+                },
+                KvPair {
+                    key: "k2".as_bytes().to_vec(),
+                    value: "value2".as_bytes().to_vec(),
+                },
+                KvPair {
+                    key: "k3".as_bytes().to_vec(),
+                    value: "v3".as_bytes().to_vec(),
+                },
+            ],
+            nonce: 1,
+            chain_id: ChainId::MainNet.into(),
+            chain_role: ChainRole::StorageShardChain.into(),
+            gas_price: 1,
+            start_gas: 2,
+        };
+        let mut buf = BytesMut::with_capacity(1024 * 4);
+        mutation.encode(&mut buf);
+        let buf = buf.freeze();
+        let signature: Secp256k1Signature = kp.sign(buf.as_ref());
+        let request = WriteRequest {
+            signature: signature.as_ref().to_vec(),
+            mutation: buf.as_ref().to_vec(),
+            public_key: kp.public().as_ref().to_vec(),
+        };
+        let mut buf = BytesMut::with_capacity(1024 * 4);
+        request.encode(&mut buf);
+        let buf = buf.freeze();
+        let mutation_encoded = hex::encode_upper(buf.as_ref());
+        println!("{}", mutation_encoded);
+        let (app, driver) = KeyValueStoreApp::new();
+        let server = ServerBuilder::default().bind("127.0.0.1:0", app).unwrap();
+        let server_addr = server.local_addr();
+        thread::spawn(move || driver.run());
+        thread::spawn(move || server.listen());
+        let mut client = ClientBuilder::default().connect(server_addr).unwrap();
+        client
+            .deliver_tx(RequestDeliverTx {
+                tx: mutation_encoded.into(),
+            })
+            .unwrap();
+        client.commit().unwrap();
+        let res = client
+            .query(RequestQuery {
+                data: "k1".as_bytes().to_owned(),
+                path: "".to_string(),
+                height: 0,
+                prove: false,
+            })
+            .unwrap();
+        assert_eq!(res.value, "value1".as_bytes().to_owned());
+        let res = client
+            .query(RequestQuery {
+                data: "k2".as_bytes().to_owned(),
+                path: "".to_string(),
+                height: 0,
+                prove: false,
+            })
+            .unwrap();
+        assert_eq!(res.value, "value2".as_bytes().to_owned());
+        let res = client
+            .query(RequestQuery {
+                data: "k3".as_bytes().to_owned(),
+                path: "".to_string(),
+                height: 0,
+                prove: false,
+            })
+            .unwrap();
+        assert_eq!(res.value, "v3".as_bytes().to_owned());
+
+
+        let res = client
+            .query(RequestQuery {
+                data: "0dce49e41905e6c0c5091adcedee2dee524a3b06".as_bytes().to_owned(),
+                path: "bill".to_string(),
+                height: 0,
+                prove: false,
+            })
+            .unwrap();
+        assert_eq!(res.value, "14".as_bytes().to_owned());
     }
 }
