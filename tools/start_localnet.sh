008e7b867bc6fc45c8b1c8de79cf45e2194946d3
imotai <codego.me@gmail.com>
2022-11-07 00:52:42 +0800

    121 support restarting abci and recover root hash and height (#124)
    * feat: update readme
    
    * fix: remove records

--- tools/start_localnet.sh
+++ tools/start_localnet.sh
@@ -3,8 +3,15 @@
 # start_localnet.sh
 
 test_dir=`pwd`
+if [ -e ./tendermint ]
+then
+    echo "tendermint_0.34.22_linux_amd64.tar.gz exist"
+else
+    wget https://github.com/tendermint/tendermint/releases/download/v0.34.22/tendermint_0.34.22_linux_amd64.tar.gz
+    tar -zxf tendermint_0.34.22_linux_amd64.tar.gz
+fi
 
-killall db3
+killall -s 9 db3
 test -d db && rm -rf db
 ../target/debug/db3 node -r 1000000 >db3.log 2>&1  &
 sleep 1
