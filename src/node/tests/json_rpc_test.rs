722d2e8c2a868e58d7c344b8ca431f4d9da55bd6
ljshwyykl <ljs@gmail.com>
2022-11-28 17:21:39 +0800

    add rpc test

---
+++ src/node/tests/json_rpc_test.rs
@@ -0,0 +1,71 @@
+mod json_rpc_test {
+    use actix_cors::Cors;
+    use actix_web::{test, web, App};
+    use db3_node::auth_storage::AuthStorage;
+    use db3_node::context::Context;
+    use db3_node::json_rpc::Response;
+    use db3_node::json_rpc_impl;
+    use db3_node::node_storage::NodeStorage;
+    use merk::Merk;
+    use serde_json::Number;
+    use serde_json::Value;
+    use std::sync::Arc;
+    use std::sync::Mutex;
+    use tendermint_rpc::HttpClient;
+
+    pub async fn init_context() -> Context {
+        let db_path = "./db";
+        let tm_port = "26657".to_string();
+        let merk = Merk::open(&db_path).unwrap();
+        let node_store = Arc::new(Mutex::new(Box::pin(NodeStorage::new(AuthStorage::new(
+            merk,
+        )))));
+
+        let tm_addr = format!("http://127.0.0.1:{}", tm_port);
+        let client = HttpClient::new(tm_addr.as_str()).unwrap();
+        let context = Context {
+            node_store: node_store.clone(),
+            client,
+        };
+
+        context
+    }
+
+    #[actix_web::test]
+    async fn test_atest_blocks() {
+        let context = init_context().await;
+
+        let app = test::init_service({
+            let cors = Cors::default()
+                .allow_any_origin()
+                .allow_any_method()
+                .allow_any_header()
+                .max_age(3600);
+            App::new()
+                .app_data(web::Data::new(context.clone()))
+                .wrap(cors)
+                .service(web::resource("/").route(web::post().to(json_rpc_impl::rpc_router)))
+        })
+        .await;
+
+        let body = serde_json::json!(
+            {
+                "method": "latest_blocks",
+                "params": [],
+                "id": 1,
+                "jsonrpc": "2.0"
+            }
+        );
+
+        let req = test::TestRequest::post()
+            .uri("/")
+            .set_json(&body)
+            .to_request();
+        let resp: Response = test::call_and_read_body_json(&app, req).await;
+
+        assert_eq!(resp.jsonrpc, "2.0".to_string());
+        assert_eq!(resp.error.is_some(), false);
+        assert_eq!(resp.id, Value::Number(Number::from(1)));
+        assert!(resp.result["block_metas"].is_array());
+    }
+}
