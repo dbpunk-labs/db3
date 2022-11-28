71b4594dfaedc23160d380b90938164abd2f33b7
imotai <codego.me@gmail.com>
2022-11-28 22:22:54 +0800

    feat: add gen proto tools

---
+++ tools/gen_proto_js.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+#
+# gen_proto.sh
+
+protoc -I=src/proto/proto db3_base.proto \
+--js_out=import_style=commonjs:src/db3js/src/pkg \
+--grpc-web_out=import_style=typescript,mode=grpcwebtext:src/db3js/src/pkg
+
+protoc -I=src/proto/proto db3_mutation.proto \
+--js_out=import_style=commonjs:src/db3js/src/pkg \
+--grpc-web_out=import_style=typescript,mode=grpcwebtext:src/db3js/src/pkg
+
+protoc -I=src/proto/proto db3_bill.proto \
+--js_out=import_style=commonjs:src/db3js/src/pkg \
+--grpc-web_out=import_style=typescript,mode=grpcwebtext:src/db3js/src/pkg
+
+protoc -I=src/proto/proto db3_account.proto \
+--js_out=import_style=commonjs:src/db3js/src/pkg \
+--grpc-web_out=import_style=typescript,mode=grpcwebtext:src/db3js/src/pkg
+
+
+protoc -I=src/proto/proto db3_node.proto \
+--js_out=import_style=commonjs:src/db3js/src/pkg \
+--grpc-web_out=import_style=typescript,mode=grpcwebtext:src/db3js/src/pkg
+
+
