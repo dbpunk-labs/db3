6bbbc2a99abe7af7fd0fce4c11e9a3c3f359689d
zhaojun <zhaojunbest@gmail.com>
2022-12-05 14:50:35 +0800

    querySession dev

--- src/db3js/src/lib/db3.test.ts
+++ src/db3js/src/lib/db3.test.ts
@@ -6,7 +6,6 @@ global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
 
 describe("test db3js api", () => {
-	const db3_instance = new DB3("http://127.0.0.1:26659");
 	async function getSign() {
 		const [sk, public_key] = await generateKey();
 
@@ -19,25 +18,81 @@ describe("test db3js api", () => {
 	}
 
 	test("test submitMutation", async () => {
+		const db3_instance = new DB3("http://127.0.0.1:26659");
 		const _sign = await getSign();
 		const result = await db3_instance.submitMutaition(
 			{
 				ns: "my_twitter",
 				gasLimit: 10,
-				data: { key123: "value123" },
+				data: { test1: "value123" },
 			},
 			_sign,
 		);
 		expect(result).toBeDefined();
 	});
 	test("test openQuerySession", async () => {
+		const db3_instance = new DB3("http://127.0.0.1:26659");
 		const _sign = await getSign();
 		try {
-			const res = await db3_instance.openQuerySession(_sign);
-			console.log(res);
-			expect(res).toBeDefined();
+			const { sessionToken } = await db3_instance.openQuerySession(_sign);
+			expect(typeof sessionToken).toBe("string");
+		} catch (error) {
+			throw error;
+		}
+	});
+	test("test getKey", async () => {
+		const db3_instance = new DB3("http://127.0.0.1:26659");
+		const _sign = await getSign();
+		try {
+			await db3_instance.submitMutaition(
+				{
+					ns: "my_twitter",
+					gasLimit: 10,
+					data: { key123: "value123" },
+				},
+				_sign,
+			);
+			const { sessionToken } = await db3_instance.openQuerySession(_sign);
+			await new Promise((r) => setTimeout(r, 2000));
+			const queryRes = await db3_instance.getKey({
+				ns: "my_twitter",
+				keyList: ["key123"],
+				sessionToken,
+			});
+			expect(queryRes.batchGetValues?.valuesList[0].value).toBe(
+				"value123",
+			);
+		} catch (error) {
+			throw error;
+		}
+	});
+	test("test db3 submit data and query data", async () => {
+		const db3_instance = new DB3("http://127.0.0.1:26659");
+		const _sign = await getSign();
+		try {
+			await db3_instance.submitMutaition(
+				{
+					ns: "my_twitter",
+					gasLimit: 10,
+					data: { test2: "value123" },
+				},
+				_sign,
+			);
+			await new Promise((r) => setTimeout(r, 2000));
+			const { sessionToken } = await db3_instance.openQuerySession(_sign);
+			const queryRes = await db3_instance.getKey({
+				ns: "my_twitter",
+				keyList: ["test2"],
+				sessionToken,
+			});
+			expect(queryRes.batchGetValues?.valuesList[0].value).toBe(
+				"value123",
+			);
+			const closeRes = await db3_instance.closeQuerySession(_sign);
+			expect(closeRes).toBeDefined();
 		} catch (error) {
 			console.error(error);
+			throw error;
 		}
 	});
 });
