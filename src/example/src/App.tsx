244331b5241fd10bb52dfc7aa04d09cda39051aa
zhaojun <zhaojunbest@gmail.com>
2022-12-05 10:31:43 +0800

    update demo

--- src/example/src/App.tsx
+++ src/example/src/App.tsx
@@ -8,9 +8,10 @@ const db3_instance = new DB3("http://locahost:26659");
 
 function App() {
 	const [res, submitMutation] = useAsyncFn(async () => {
-		const [sk, pk] = await generateKey();
-		function _sign(data: Uint8Array) {
-			return sign(data, sk);
+		const [sk, public_key] = await generateKey();
+
+		async function _sign(data: Uint8Array) {
+			return [await sign(data, sk), public_key];
 		}
 
 		try {
