fd0d7ad9ae23985236f639fba0b5a314c2e30ca1
imotai <codego.me@gmail.com>
2023-01-03 13:59:16 +0800

    fix: remove db3js

--- src/db3js/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './lib/db3'
-export * from './lib/keys'
-export * from './lib/doc_store'
--- src/db3js/tsup.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'tsup'
-
-export default defineConfig({
-    entry: ['src/index.ts'],
-    shims: true,
-    format: ['esm', 'cjs', 'iife'],
-})
