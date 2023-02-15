8d642ae6b78ffcf8c273f245d1057ef97c18a137
imotai <codego.me@gmail.com>
2023-02-15 19:27:50 +0800

    fix: add token.rs

---
+++ src/types/src/token.rs
@@ -0,0 +1,20 @@
+//
+// token.rs
+// Copyright (C) 2023 db3.network Author imotai <codego.me@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+pub const TOKEN_UNIT: [&str; 2] = ["db3", "tai"];
+// 1 db3 = 1000_000_000 tai
+pub const TOKEN_COVERSION: u64 = 1000_000_000;
