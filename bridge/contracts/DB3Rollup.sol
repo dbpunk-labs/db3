50caefde545fcabcafe656f9eae3759c118aacd7
imotai <codego.me@gmail.com>
2023-05-23 19:53:37 +0800

    feat: add settle function

--- bridge/contracts/DB3Rollup.sol
+++ bridge/contracts/DB3Rollup.sol
@@ -2,14 +2,19 @@
 pragma solidity ^0.8.9;
 
 import "./DB3Token.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract DB3Rollup {
     Db3Token private _tokenContract;
     // the locked balance of an address
     mapping(address => uint256) private _balances;
-    event Deposit(address _from, uint256 amount);
-    constructor(Db3Token tokenContract) {
+    bytes32 private _root;
+    event Deposit(address from, uint256 amount);
+    event Settlement(address owner, uint256 amount);
+
+    constructor(Db3Token tokenContract, bytes32 root) {
         _tokenContract = tokenContract;
+        _root = root;
     }
 
     function deposit(uint256 amount) public payable returns (bool) {
@@ -21,5 +26,64 @@ contract DB3Rollup {
 
     function getLockedBalance(address owner) public view returns (uint256) {
         return _balances[owner];
+    }
+
+    function _conversion(
+        bytes[] calldata states
+    ) private pure returns (bytes32[] memory leaves) {
+        for (uint i = 0; i < states.length; i++) {
+            (address account, uint256 balance) = abi.decode(
+                states[i],
+                (address, uint256)
+            );
+            bytes32 leaf = keccak256(abi.encodePacked(account, balance));
+            leaves[i] = leaf;
+        }
+    }
+
+    function verifyStates(
+        bytes32[] calldata proof,
+        bool[] calldata proofFlags,
+        bytes32 preRootHash,
+        bytes32 postRootHash,
+        bytes[] calldata states
+    ) public view returns (bool) {
+        require(preRootHash == _root);
+        bytes32[] memory leaves = _conversion(states);
+        return
+            MerkleProof.multiProofVerifyCalldata(
+                proof,
+                proofFlags,
+                postRootHash,
+                leaves
+            );
+    }
+
+    function processUpdateStates(
+        bytes32[] calldata proof,
+        bool[] calldata proofFlags,
+        bytes32 preRootHash,
+        bytes32 postRootHash,
+        bytes[] calldata states
+    ) public returns (bool) {
+        require(preRootHash == _root);
+        bytes32[] memory leaves = _conversion(states);
+        require(
+            MerkleProof.multiProofVerifyCalldata(
+                proof,
+                proofFlags,
+                postRootHash,
+                leaves
+            )
+        );
+        for (uint i = 0; i < states.length; i++) {
+            (address account, uint256 balance) = abi.decode(
+                states[i],
+                (address, uint256)
+            );
+            _balances[account] = balance;
+        }
+        _root = postRootHash;
+        return true;
     }
 }
---
+++ bridge/format.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+#
+# format.sh
+
+
+
+yarn prettier --write contracts
