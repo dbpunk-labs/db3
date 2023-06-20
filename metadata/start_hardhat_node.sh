9959fd28398bfe4101ddb0b71b7103fbebf892f9
Rock <35133768+Rock-liyi@users.noreply.github.com>
2023-06-20 11:27:29 +0800

    Feat/smart contracts for storing network infomation (#491)
    * add smart contracts for storing network information
    
    * fix solidity ^0.8.0 to solidity ^0.8.9
    
    * fix no arweave tx is required when registering network
    
    * Add check code
    
    * Add updateNetworkIndexNodes function and Add permission check for registerIndexNode
    
    * fix updateNetworkIndexNodes registration.rollupNodeAddress to registration.admin
    
    * fix updateNetworkIndexNodes registration.rollupNodeAddress to registration.admin
    
    * fix updateNetworkIndexNodes registration.rollupNodeAddress to registration.admin
    
    * fix updateNetworkIndexNodes registration.rollupNodeAddress to registration.admin

--- metadata/start_hardhat_node.sh
+++ metadata/start_hardhat_node.sh
@@ -2,4 +2,7 @@
 #
 # start_hardhat_node.sh
 
-npx hardhat node --fork https://eth-mainnet.g.alchemy.com/v2/VBNepFcLV_68pB3AyfplsdQWBxr8CKd4
+# npx hardhat node --fork https://eth-mainnet.g.alchemy.com/v2/VBNepFcLV_68pB3AyfplsdQWBxr8CKd4
+
+npx hardhat node --fork https://eth-testnet.g.alchemy.com/v2/VBNepFcLV_68pB3AyfplsdQWBxr8CKd4
+
