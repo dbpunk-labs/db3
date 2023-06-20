3b2a9e60833d7bb6ec6fa05d0c9abbbf4439b624
imotai <codego.me@gmail.com>
2023-06-20 10:40:43 +0800

    feat: add docker build

--- tools/ar_miner.sh
+++ tools/ar_miner.sh
@@ -8,7 +8,7 @@
 
 npx arlocal >arlocal.log 2>&1 &
 sleep 1
-curl http://127.0.0.1:1984/mint/2FqRYpb7tnjGBkXj9F_ChIZHUWoWhj6-BYMsg0TdsEg/10000000000000000000000
+curl http://127.0.0.1:1984/mint/$1/1000000000000000000
 while true
 do
     curl http://127.0.0.1:1984/mine
