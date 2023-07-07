f8ad27102c109deaafbc604ec0c9be8fedeadcc6
imotai <codego.me@gmail.com>
2023-07-07 23:54:45 +0800

    test: add exeception test

--- metadata/test/db3_metastore_network_register_test.ts
+++ metadata/test/db3_metastore_network_register_test.ts
@@ -16,7 +16,6 @@ describe("DB3MetaStore", function () {
         it("registers a new network ", async function () {
             const hello = ethers.utils.formatBytes32String("hello");
             let eventLibABI = await ethers.getContractAt("Events", metaStore.address, deployer)
-
             await expect(metaStore
                 .connect(deployer)
                 .registerDataNetwork(
@@ -29,6 +28,7 @@ describe("DB3MetaStore", function () {
             const dataNetwork = await metaStore.getDataNetwork(1);
             expect(deployer.address).to.equal(dataNetwork.admin);
             expect(deployer.address).to.equal(dataNetwork.rollupNodeAddress);
+            await expect(metaStore.getDataNetwork(2)).to.revertedWith("Data Network is not registered");
         });
     });
 });
