6b4e293267da19096f7e41533d9b89c01660146b
imotai <codego.me@gmail.com>
2023-07-08 17:39:44 +0800

    test: add test file

---
+++ metadata/test/db3_metastore_network_create_collection_test.ts
@@ -0,0 +1,145 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { DB3MetaStore, Events } from "../typechain-types";
+
+describe("DB3MetaStore", function () {
+  let metaStore: DB3MetaStore;
+  let deployer: any;
+  let sender: any;
+  beforeEach(async function () {
+    [deployer, sender] = await ethers.getSigners();
+    const MetaStore = await ethers.getContractFactory("DB3MetaStore");
+    metaStore = await MetaStore.deploy();
+    await metaStore.deployed();
+  });
+  describe("createCollection", function () {
+    it("create collection invalid network id", async function () {
+      const hello = ethers.utils.formatBytes32String("hello");
+      await expect(
+        metaStore
+          .connect(deployer)
+          .createCollection(
+            1,
+            "0xF935E45C32C7DCc54bDDEcE5309c4313368A598A",
+            hello
+          )
+      ).to.revertedWith("Data Network is not registered");
+    });
+
+    it("create collection invalid database", async function () {
+      const eventLibABI = await ethers.getContractAt(
+        "Events",
+        metaStore.address,
+        deployer
+      );
+
+      const hello = ethers.utils.formatBytes32String("hello");
+      await expect(
+        metaStore
+          .connect(deployer)
+          .registerDataNetwork(
+            "https://rollup.com",
+            deployer.address,
+            ["https://index-node-1.com", "https://index-node-2.com"],
+            [sender.address, deployer.address],
+            hello
+          )
+      )
+        .to.emit(eventLibABI, "CreateNetwork")
+        .withArgs(deployer.address, 1);
+      await expect(
+        metaStore
+          .connect(deployer)
+          .createCollection(
+            1,
+            "0xF935E45C32C7DCc54bDDEcE5309c4313368A598A",
+            hello
+          )
+      ).to.revertedWith("Database was not found");
+    });
+
+    it("create collection invalid permission", async function () {
+      const eventLibABI = await ethers.getContractAt(
+        "Events",
+        metaStore.address,
+        deployer
+      );
+
+      const hello = ethers.utils.formatBytes32String("hello");
+      await expect(
+        metaStore
+          .connect(deployer)
+          .registerDataNetwork(
+            "https://rollup.com",
+            deployer.address,
+            ["https://index-node-1.com", "https://index-node-2.com"],
+            [sender.address, deployer.address],
+            hello
+          )
+      )
+        .to.emit(eventLibABI, "CreateNetwork")
+        .withArgs(deployer.address, 1);
+      await expect(metaStore.connect(deployer).createDocDatabase(1, hello))
+        .to.emit(eventLibABI, "CreateDatabase")
+        .withArgs(
+          deployer.address,
+          1,
+          "0xF935E45C32C7DCc54bDDEcE5309c4313368A598A"
+        );
+      await expect(
+        metaStore
+          .connect(sender)
+          .createCollection(
+            1,
+            "0xF935E45C32C7DCc54bDDEcE5309c4313368A598A",
+            hello
+          )
+      ).to.revertedWith("You must the database sender");
+    });
+
+    it("create collection smoke test", async function () {
+      const hello = ethers.utils.formatBytes32String("hello");
+      const eventLibABI = await ethers.getContractAt(
+        "Events",
+        metaStore.address,
+        deployer
+      );
+      await expect(
+        metaStore
+          .connect(deployer)
+          .registerDataNetwork(
+            "https://rollup.com",
+            deployer.address,
+            ["https://index-node-1.com", "https://index-node-2.com"],
+            [sender.address, deployer.address],
+            hello
+          )
+      )
+        .to.emit(eventLibABI, "CreateNetwork")
+        .withArgs(deployer.address, 1);
+      await expect(metaStore.connect(deployer).createDocDatabase(1, hello))
+        .to.emit(eventLibABI, "CreateDatabase")
+        .withArgs(
+          deployer.address,
+          1,
+          "0xF935E45C32C7DCc54bDDEcE5309c4313368A598A"
+        );
+      await expect(
+        metaStore
+          .connect(deployer)
+          .createCollection(
+            1,
+            "0xF935E45C32C7DCc54bDDEcE5309c4313368A598A",
+            hello
+          )
+      )
+        .to.emit(eventLibABI, "CreateCollection")
+        .withArgs(
+          deployer.address,
+          1,
+          "0xF935E45C32C7DCc54bDDEcE5309c4313368A598A",
+          hello
+        );
+    });
+  });
+});
