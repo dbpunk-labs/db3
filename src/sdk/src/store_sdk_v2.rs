0974e4a69927cecfcadb32ef51d2fab10a301aef
Chen22 <jingchen2222@gmail.com>
2023-07-20 18:23:51 -0700

    feat: indexer cold start (WIP) (#566)
    * add unit test for upload ut
    
    * sleep 3 second before request token and print balance after upload
    
    * revert start local setting
    
    * upload logs when fail
    
    * update ar file ut with balance log
    
    * upload logs when test coverage fail
    
    * change test task order
    
    * fix contract.log
    
    * revert ci workflow
    
    * ignore ut for cicd
    
    * ignore test in ci feature enable
    
    * support indexer cold start
    
    * fix indexer recover
    
    * fix: format the code
    
    * fix fetch block
    
    * remove unused code
    
    ---------
    
    Co-authored-by: imotai <codego.me@gmail.com>

--- src/sdk/src/store_sdk_v2.rs
+++ src/sdk/src/store_sdk_v2.rs
@@ -28,6 +28,7 @@ use ethers::core::types::{
 };
 use ethers::prelude::{LocalWallet, Signer};
 use prost::Message;
+use prost_types::field_descriptor_proto::Type::Int64;
 use std::collections::BTreeMap;
 use std::sync::Arc;
 use tonic::{Status, Streaming};
@@ -38,6 +39,7 @@ pub struct StoreSDKV2 {
     types: Types,
 }
 
+const MAX_BLOCK_ID: u64 = u64::MAX;
 impl StoreSDKV2 {
     pub fn new(
         client: Arc<StorageNodeV2Client<tonic::transport::Channel>>,
@@ -105,16 +107,23 @@ impl StoreSDKV2 {
         client.subscribe(req).await
     }
 
+    pub async fn get_blocks(
+        &self,
+        block_start: u64,
+        block_end: u64,
+    ) -> Result<tonic::Response<BlockResponseV2>, Status> {
+        let req = BlockRequestV2 {
+            block_start,
+            block_end,
+        };
+        let mut client = self.client.as_ref().clone();
+        client.get_block(req).await
+    }
     pub async fn get_block_by_height(
         &self,
         height: u64,
     ) -> Result<tonic::Response<BlockResponseV2>, Status> {
-        let req = BlockRequestV2 {
-            block_start: height,
-            block_end: height + 1,
-        };
-        let mut client = self.client.as_ref().clone();
-        client.get_block(req).await
+        self.get_blocks(height, height + 1).await
     }
 }
 
